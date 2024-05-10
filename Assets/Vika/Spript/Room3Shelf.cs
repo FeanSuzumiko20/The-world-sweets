6857a917c351e1cc0da6cdc62fd84fc201765630
FeanSuzumiko <159698007+FeanSuzumiko20@users.noreply.github.com>
2024-05-10 20:24:27 +0300

    2

---
+++ Assets/Vika/Spript/Room3Shelf.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room3Shelf : MonoBehaviour
+{   public GameObject text;
+    public GameObject Hint;
+    private void OnTriggerStay(Collider other)
+    {
+        var player = other.gameObject.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            Hint.SetActive(true);
+            if (Input.GetKey(KeyCode.E))
+            {
+                text.SetActive(true);
+
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Hint.SetActive(false);
+        text.SetActive(false);
+    }
+}
+
---
+++ Assets/Vika/Spript/Spikes2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes2 : MonoBehaviour
+{
+    public int secondsoff = 100;
+    public int secondson = 200;
+    public int seconds = 100;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (seconds == secondsoff)
+        {
+            transform.position = transform.position + new Vector3(0, -2f, 0);
+
+        }
+        if (seconds == secondson)
+        {
+            transform.position = transform.position + new Vector3(0, 2f, 0);
+            seconds = 0;
+        }
+        seconds++;
+
+
+
+    }
+}
