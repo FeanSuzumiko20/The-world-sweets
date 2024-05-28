880a464b6d6846443f89ad2c9b9b4c78fdea1472
FeanSuzumiko <159698007+FeanSuzumiko20@users.noreply.github.com>
2024-05-28 20:12:35 +0300

    Few subdialogues

---
+++ Assets/Vika/Spript/DialogueTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public GameObject Dia;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(waiter());
+    }
+
+    // Update is called once per frame
+    IEnumerator waiter()
+    {
+        Dia.SetActive(true);
+        yield return new WaitForSeconds(3);
+        Dia.SetActive(false);
+    }
+}
